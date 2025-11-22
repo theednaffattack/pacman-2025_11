c5aee353bdab94ffb8c8fc9a2c15b6891cabdc12
Eddie Naff <eddienaff@gmail.com>
2025-11-21 19:47:49 -0800

    Move images

--- src/pacman.ts
+++ src/pacman.ts
@@ -1,7 +1,7 @@
 import "./pacman.css";
 
 // board
-let board: HTMLElement | null;
+let board;
 const rowCount = 21;
 const columnCount = 19;
 const tileSize = 32;
@@ -9,10 +9,17 @@ const boardHeight = rowCount * tileSize;
 const boardWidth = columnCount * tileSize;
 let context;
 
+//images
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+
 window.onload = function () {
   board = document.getElementById("board");
-  if (board) {
+  if (board && board instanceof HTMLCanvasElement) {
     board.style.height = `${boardHeight}px`;
     board.style.width = `${boardWidth}px`;
+    context = board.getContext("2d");
   }
 };
